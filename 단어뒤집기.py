df55620a48a00c482ad62cc9f8591978b3ce83b0
power30111 <112193464+power30111@users.noreply.github.com>
2022-10-13 00:58:30 +0900

    Create 단어뒤집기.py

---
+++ 단어뒤집기.py
@@ -0,0 +1,16 @@
+#https://www.acmicpc.net/problem/9093
+import sys
+from collections import deque   #deque
+from itertools import permutations
+input = sys.stdin.readline
+
+T = int(input())    #입력받는 횟수.
+array=deque()
+A=[]
+for i in range(T):
+    A=list(input().split())
+    for j in range(len(A)):
+        for k in range(len(A[j])-1,-1,-1):
+            print(A[j][k],end="")
+        print(end=" ")
+    print()
