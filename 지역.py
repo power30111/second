313ca469b5cd25ab96232f1be5544d01b7aa18a3
(IHJ) <(power30111@naver.com)>
2023-01-17 22:18:10 +0900

    baekjoon

--- 지역.py
+++ 지역.py
@@ -2,11 +2,13 @@ import sys
 input=sys.stdin.readline
 
 N,M=map(int,input().split())
-
+size=1
 array=[[]for i in range(N+1)]
 for i in range(1,N):
     array[i].append(i+1)
 for i in range(M):
     x,y=map(int,input().split())
     array[x].append(y)
-print(array)
+
+def DFS():
+    
